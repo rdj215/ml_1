a3dfddc3aa371e8919c212d6493c81e02c8777df
Ryan Jamison <ryanjamison26@gmail.com>
2025-03-24 21:25:15 -0400

    setting up project folder structure

---
+++ model/model.py
@@ -0,0 +1,19 @@
+import pickle 
+from sklearn.datasets import fetch_california_housing
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+
+#Load housing data 
+data = fetch_california_housing()
+
+X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, test_size=0.2, random_state=40)
+
+
+#Train model
+model = RandomForestRegressor(n_estimator=100)
+model.fit(X_train, y_train)
+
+with open("/model/model.pkl", "wb") as f:
+	pickle.dump(model,f)
+
+
